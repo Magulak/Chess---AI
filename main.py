b4696712cba76678c19143fa67fb0c5a5fc2a453
Czarek dom <zdrojewski.czarek@gmail.com>
2022-09-03 12:03:34 +0200

    cleaning up

--- main.py
+++ main.py
@@ -29,81 +29,9 @@ print(list(Season))"""
 import pygame
 import sys
 
-ui = True
-
-if ui:
-    pygame.init()
-
-    size = width, height = 1320, 1240
-    square_size = 40
-    speed = [1, 1]
-    black = 0, 0, 0
-    white = 255, 255, 255
-
-    gameDisplay = pygame.display.set_mode(size)
-    boardLength = 8
-    gameDisplay.fill(white)
-
-    piece = pygame.image.load("piece.png")
-    board = pygame.image.load("Chess_Board.jpg")
-    pygame.display.set_caption('Quick Start')
-
-    piece_rect = piece.get_rect()
-    board_rect = board.get_rect()
-
-    while 1:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-
-        piece_rect = piece_rect.move(speed)
-        if piece_rect.left < 0 or piece_rect.right > width:
-            speed[0] = -speed[0]
-        if piece_rect.top < 0 or piece_rect.bottom > height:
-            speed[1] = -speed[1]
-
-        gameDisplay.fill(black)
-        gameDisplay.blit(piece, piece_rect)
-        gameDisplay.blit(board, board_rect)
-        pygame.display.flip()
-
-
-#
-#
-# for event in pygame.event.get():
-#     if event.type == pygame.QUIT: sys.exit()
-#
-# # set color with rgb
-# white, black, red = (255, 255, 255), (0, 0, 0), (255, 0, 0)
-#
-# # set display
-# gameDisplay = pygame.display.set_mode((1920, 980))
-#
-# # Size of squares
-# size = 20
-#
-# # Board length, must be even
-# boardLength = 8
-# gameDisplay.fill(white)
-#
-# cnt = 0
-# for i in range(1, boardLength + 1):
-#     for z in range(1, boardLength + 1):
-#         # check if current loop value is even
-#         if cnt % 2 == 0:
-#             pygame.draw.rect(gameDisplay, white, [size * z, size * i, size, size])
-#         else:
-#             pygame.draw.rect(gameDisplay, black, [size * z, size * i, size, size])
-#         cnt += 1
-#     # since theres an even number of squares go back one value
-#     cnt -= 1
-# # Add a nice boarder
-# pygame.draw.rect(gameDisplay, black, [size, size, boardLength * size, boardLength * size], 1)
-#
-# pygame.display.update()
-
 
 class Chess:
-    class Piece():
+    class Piece:
 
         def __init__(self, type_of_piece, coordinates):  # constructor
             print(coordinates)
@@ -285,4 +213,20 @@ class Chess:
             print("Queen")
             chess_board2 = King.moveset(0, y, x, fy, fx, chess_board2)
 
-        # if clicked button == ` then break:
+        # TODO if clicked button == ` then break:
+        pygame.init()
+
+        square_size = width, height = 1320, 1240
+        white, black, red = (255, 255, 255), (0, 0, 0), (255, 0, 0)
+
+        gameDisplay = pygame.display.set_mode(square_size)
+
+        gameDisplay.fill(white)
+        pygame.display.set_caption('Chess--AI')
+
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
+            gameDisplay.fill(white)
+
+            pygame.display.update()  # TODO give it correct parameters.
