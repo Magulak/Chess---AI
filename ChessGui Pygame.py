cbfea7c5ae797a4a32f33d64e8ba6d8c6a4548c7
Czarek dom <zdrojewski.czarek@gmail.com>
2023-01-26 20:08:33 +0100

    more comments

--- ChessGui Pygame.py
+++ ChessGui Pygame.py
@@ -8,6 +8,8 @@ def draw_chessboard(surface):
             if (i + j) % 2 == 0:
                 pygame.draw.rect(surface, black, (i * 87, j * 87, 87, 87))
 
+def handle_mouse_events(event):
+
 
 # Window size
 size = (700, 700)
@@ -33,7 +35,7 @@ image.set_alpha(200)
 # list of pieces and their position
 piece_position_list = []
 
-piece_draging = False
+piece_dragging = False
 
 
 # TODO WHITE PIECES
@@ -70,47 +72,50 @@ running = True
 
 clock = pygame.time.Clock()
 
-# create back buffer and fill it with white color
+# create back buffer and fill it with white color.
 back_buffer = pygame.Surface((screen.get_width(), screen.get_height()))
 back_buffer.fill(white)
 # TODO make so pieces 'lock in' the centre of a square.
 # TODO add more pieces.
 while running:
 
+    # Stop running if pygame window closed.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+        # Detect left mouse click-hold.
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                for piece in piece_position_list:  # Multiple object collision
+                for piece in piece_position_list:  # Consider collision for every coordinate in list.
                     if piece.collidepoint(event.pos):
-                        piece_draging = True
-                        draged_piece = piece
+                        piece_dragging = True
+                        dragged_piece = piece
                         mouse_x, mouse_y = event.pos
-                        offset_x = piece.x - mouse_x
+                        offset_x = piece.x - mouse_x  # Offset - distance between right upper corner of image and cursor
                         offset_y = piece.y - mouse_y
 
         elif event.type == pygame.MOUSEBUTTONUP:
             if event.button == 1:
-                piece_draging = False
-                draged_piece = None
+                piece_dragging = False
+                dragged_piece = None  # Dragged piece needs to be specified, otherwise all pieces react to dragging.
 
+        # Overwrite piece's current position with cursors position + offset
         elif event.type == pygame.MOUSEMOTION:
-            if piece_draging:
+            if piece_dragging:
                 for piece in piece_position_list:
                     mouse_x, mouse_y = event.pos
-                    draged_piece.x = mouse_x + offset_x
-                    draged_piece.y = mouse_y + offset_y
+                    dragged_piece.x = mouse_x + offset_x
+                    dragged_piece.y = mouse_y + offset_y
 
     # Fill the screen with white
     screen.fill(white)
 
     # Draw the chess board
     draw_chessboard(back_buffer)
-
     screen.blit(back_buffer, (0, 0))
 
+    # Draw images of chess pieces on chess board
     for piece in piece_position_list:
         screen.blit(image, piece)
 
